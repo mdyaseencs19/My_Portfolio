273311c6cb0feae46fa21d87a655de9c70924cb0
Md Yaseen Ahmed <60700271+mdyaseencs19@users.noreply.github.com>
2020-07-12 15:09:38 +0530

    Initial Commit

---
+++ build/precache-manifest.3a48fafe9211560a733217c9c37b911e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b05a64aa50e334443f297447761d112",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5690378a3a708b645de1",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "914f8d2c96799fd56e2b",
+    "url": "/static/css/main.7393c68d.chunk.css"
+  },
+  {
+    "revision": "5690378a3a708b645de1",
+    "url": "/static/js/2.cb5d5281.chunk.js"
+  },
+  {
+    "revision": "5d138673e2af8a9b93b9251c5e370fe0",
+    "url": "/static/js/2.cb5d5281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914f8d2c96799fd56e2b",
+    "url": "/static/js/main.2adf32ce.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8878f7f4f28ad040bcc900c7b9081901",
+    "url": "/static/media/banner-1.8878f7f4.png"
+  },
+  {
+    "revision": "c0120b1720334d6c54700fe580268a77",
+    "url": "/static/media/bootstrap4.c0120b17.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "17ecacbd5dc1bf4ed8e6bd24bc3f5001",
+    "url": "/static/media/materialize.17ecacbd.png"
+  },
+  {
+    "revision": "c4372e1cd0ad4c852bf8b3febea21608",
+    "url": "/static/media/yaseen.c4372e1c.jpeg"
+  }
+]);
